5b6ecc6f143757d53b163a32a55035566f327e64
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-08-22 00:00:55 +0530

    two efficient code than previous

--- 1.sum zero problem/app.js
+++ 1.sum zero problem/app.js
@@ -5,7 +5,8 @@
 
 // first solution -- O(n^2) quadratic
 let arr1 = [-5, -4, -3, -2, 0, 2, 4, 6, 8]
-let arr2 = [4, 5, 6, 7, 8, 9, -21, 0, -2, 4, -1, -2]
+// let arr2 = [4, 5, 6, 7, 8, 9, -21, 0, -2, 4, -1, -2]
+let arr2=[-21,-2,-2,0,4,4,5,6,7,8,9]
 const findSumZeroPair = (arr) => {
 
     let l = arr.length
@@ -22,3 +23,39 @@ console.log('1:', findSumZeroPair(arr1))
 console.log('2:', findSumZeroPair(arr2))
 
 // second solution --
+const findSumZeroPair2 =(arr)=>{ // only work when arr is sorted
+    for(let i=0;i<arr.length;i++){
+        let right = arr[i]
+        for(let j = arr.length-1;j>=0;j--){
+            let left = arr[j]
+            if(right + left === 0){
+                return [left,right]
+            }else if (right + left < 0){
+                break
+            }
+        }
+    } return 0
+}
+console.log('3:',findSumZeroPair2(arr1))
+console.log('3:',findSumZeroPair2(arr2))
+
+// third solution same to second but different way to write
+const findSumZeroPair3 = (arr)=>{
+    let left = 0
+    let right = arr.length -1
+    while(arr[left] < arr[right]){
+        let sum = arr[left] + arr[right]
+        if(sum === 0){
+            return [arr[left] , arr[right]]
+        }else if(sum < 0){
+            left++
+        }else{
+            right--
+        }
+    } 
+    return [0,0]
+}
+
+
+console.log('3:',findSumZeroPair3(arr1))
+console.log('3:',findSumZeroPair3(arr2))
