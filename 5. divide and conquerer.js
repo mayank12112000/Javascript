26083ddb13ed872f10b926edf685349971da6d9c
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-08-23 18:28:46 +0530

    Create 5. divide and conquerer.js

---
+++ 5. divide and conquerer.js
@@ -0,0 +1,8 @@
+// divide and conquerer technique
+// find the index of given no. in sorted array
+// [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] => 7 ->input || index 6 -> output
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let n = 7
+let binarySearch = (arr,n)=>{
+    
+}
