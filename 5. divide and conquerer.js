05e7cbdcd01644b9bb61ae091814456e507f0a0b
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-08-27 09:39:03 +0530

    Update 5. divide and conquerer.js

--- 5. divide and conquerer.js
+++ 5. divide and conquerer.js
@@ -1,6 +1,8 @@
 // divide and conquerer technique
 // find the index of given no. in sorted array
 // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] => 7 ->input || index 6 -> output
+
+// O(n)
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 let n = 7
 let binarySearch = (arr,n)=>{
@@ -10,10 +12,11 @@ let binarySearch = (arr,n)=>{
     let midIdx = Math.floor((min + max)/2)
     let midValue = arr[midIdx]
     if(n>midValue){
-        min = midIdx
+        min = midIdx +1
     }else if(n<midValue){
-        max = midIdx
+        max = midIdx - 1
     }else return midIdx
    }
+   return -1
 }
-console.log(binarySearch(arr,7))
+console.log(binarySearch(arr,4))
