dff73359de5c54f59ed11d5ff3d0ed7abf62f678
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-08-27 09:34:15 +0530

    Update 5. divide and conquerer.js

--- 5. divide and conquerer.js
+++ 5. divide and conquerer.js
@@ -4,5 +4,16 @@
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 let n = 7
 let binarySearch = (arr,n)=>{
-    
+    let min = 0;
+    let max = arr.length -1
+   while(min <= max){
+    let midIdx = Math.floor((min + max)/2)
+    let midValue = arr[midIdx]
+    if(n>midValue){
+        min = midIdx
+    }else if(n<midValue){
+        max = midIdx
+    }else return midIdx
+   }
 }
+console.log(binarySearch(arr,7))
